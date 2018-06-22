686bd2187186d330ad061d7adb1b5e3dddc00104
Joey Reed <joeyrreed@gmail.com>
2018-06-22 16:10:24 -0700

    jrr

--- app/index.js
+++ app/index.js
@@ -1,7 +1,4 @@
 import './styles.css';
-import chart from './chart.js'
-
-
 import * as d3 from 'd3';
 
 // Canvas margin 
@@ -17,24 +14,28 @@ const chart  = document.getElementById('chart');
 const height = chart.clientHeight;
 const width  = chart.clientWidth;
 
-const canvas  = document.querySelector('canvas');
-canvas.height = height - margin.t - margin.b;
-canvas.width  = width - margin.l - margin.r;
-canvas.style.top  = `${margin.t}px`, 
-canvas.style.left = `${margin.l}px`;
-const ctx = canvas.getContext('2d');
+//const canvas  = document.querySelector('canvas');
+//canvas.height = height - margin.t - margin.b;
+//canvas.width  = width - margin.l - margin.r;
+//canvas.style.top  = `${margin.t}px`, 
+//canvas.style.left = `${margin.l}px`;
+//const ctx = canvas.getContext('2d');
 
 
-const l = 0;//margin.l;
-const r = canvas.width;//width - margin.r;
-const t = 0;//margin.t;
-const b = canvas.height;//height-margin.b;
+const l = 0;
+const r = width - margin.l - margin.r;
+const t = 0;
+const b = height - margin.t - margin.b;
 
 const xScale = d3.scaleLinear().domain([0, 100]).range([l, r]);
 const yScale = d3.scaleLinear().domain([-1, 1]).range([b, t]);
 
 const xAxis = d3.axisBottom(xScale);
 const yAxis = d3.axisLeft(yScale);
+const line  = d3.line()
+    .x(d => xScale(d[0]))
+    .y(d => yScale(d[1]));
+
 
 d3.select('.x.axis')
     .attr('transform', `translate(${margin.l},${height-margin.b})`)
@@ -44,21 +45,34 @@ d3.select('.y.axis')
     .attr('transform', `translate(${margin.l},${margin.t})`)
     .call(yAxis);
 
-// signal
+const data = [];
 const fs = 1000;
 const fc = 50;
-const n  = 100;
-ctx.lineCap  = 'round';  
-ctx.lineJoin = 'round';
-ctx.strokeStyle = 'teal';
-ctx.lineWidth=1;
-           
-           
-ctx.beginPath();
-ctx.moveTo(xScale(0), yScale(0));
-for (let i=1; i < n; i++) {
-    const x = i;
-    const y = Math.sin(2 * Math.PI * (fc / fs) * i);
-    ctx.lineTo(xScale(x), yScale(y));  
+for (let i = 0; i < 100; i++) {
+    data.push([
+        i, Math.sin(2 * Math.PI * (fc / fs) * i)    
+    ])
 }
-ctx.stroke(); 
+
+const el = document.querySelector('.line');
+el.setAttribute('d', line(data));
+el.setAttribute('transform', `translate(${margin.l},${margin.t})`);
+
+// signal
+//const fs = 1000;
+//const fc = 50;
+//const n  = 100;
+//ctx.lineCap  = 'round';  
+//ctx.lineJoin = 'round';
+//ctx.strokeStyle = 'teal';
+//ctx.lineWidth=1;
+           
+           
+//ctx.beginPath();
+//ctx.moveTo(xScale(0), yScale(0));
+//for (let i=1; i < n; i++) {
+//    const x = i;
+//    const y = Math.sin(2 * Math.PI * (fc / fs) * i);
+//    ctx.lineTo(xScale(x), yScale(y));  
+//}
+//ctx.stroke(); 
